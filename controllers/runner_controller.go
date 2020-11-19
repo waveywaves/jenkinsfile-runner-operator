3a451095a8a4f4e2adbe13e3a2cc342e1be7d6f9
Vibhav Bobade <vibhav.bobde@gmail.com>
2020-11-19 05:41:46 +0530

    Add runnerimage CR and runner init impl

--- controllers/runner_controller.go
+++ controllers/runner_controller.go
@@ -18,13 +18,25 @@ package controllers
 
 import (
 	"context"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
 	"github.com/go-logr/logr"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 
-	jenkinsiov1alpha1 "github.com/waveywaves/jenkinsfile-runner-operator/api/v1alpha1"
+	"github.com/waveywaves/jenkinsfile-runner-operator/api/v1alpha1"
+)
+
+const (
+	JenkinsfileRunnerContainerName = "runner"
+	// Default attributes for runner container
+	DefaultCPULimit   = "100m"
+	DefaultMEMLimit   = "100Mi"
+	DefaultCPURequest = "50m"
+	DefaultMEMRequest = "50Mi"
 )
 
 // RunnerReconciler reconciles a Runner object
@@ -38,16 +50,61 @@ type RunnerReconciler struct {
 // +kubebuilder:rbac:groups=jenkinsfilerunner.io,resources=runners/status,verbs=get;update;patch
 
 func (r *RunnerReconciler) Reconcile(req ctrl.Request) (ctrl.Result, error) {
-	_ = context.Background()
-	_ = r.Log.WithValues("runner", req.NamespacedName)
+	ctx := context.Background()
+	runnerLogger := r.Log.WithValues("runnerimage", req.NamespacedName)
 
-	// your logic here
+	// Fetch the Jenkinsfile Runner Image instance
+	runnerInstance := &v1alpha1.Runner{}
+	err := r.Client.Get(ctx, req.NamespacedName, runnerInstance)
+	if err != nil {
+		if apierrors.IsNotFound(err) {
+			// Request object not found, could have been deleted after reconcile request.
+			// Owned objects are automatically garbage collected. For additional cleanup logic use finalizers.
+			// Return and don't requeue
+			return ctrl.Result{}, nil
+		}
+		// Error reading the object - requeue the request.
+		return ctrl.Result{}, err
+	}
+	runnerLogger.Info("Runner instance with name " + runnerInstance.Name + " found")
+
+	runnerPod := r.getRunnerPod(req)
+	err = r.Client.Create(context.TODO(), runnerPod)
+	if err != nil {
+		return ctrl.Result{}, err
+	}
 
 	return ctrl.Result{}, nil
 }
 
+func (r *RunnerReconciler) getRunnerPod(req ctrl.Request) *corev1.Pod {
+	return &corev1.Pod{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      getRunnerPodName(req.Name),
+			Namespace: req.Namespace,
+		},
+		Spec: corev1.PodSpec{
+			Containers: []corev1.Container{
+				{
+					Name:            JenkinsfileRunnerContainerName,
+					Image:           "jenkins/jenkinsfile-runner:latest",
+					ImagePullPolicy: corev1.PullIfNotPresent,
+					Command:         []string{"/bin/sh", "-c", "--"},
+					Args:            []string{"while true; do sleep 30; done;"},
+					Stdin:           true,
+					TTY:             true,
+				},
+			},
+		},
+	}
+}
+
+func getRunnerPodName(runnerName string) string {
+	return "jenkinsfile-runner-" + runnerName
+}
+
 func (r *RunnerReconciler) SetupWithManager(mgr ctrl.Manager) error {
 	return ctrl.NewControllerManagedBy(mgr).
-		For(&jenkinsiov1alpha1.Runner{}).
+		For(&v1alpha1.Runner{}).
 		Complete(r)
 }
--- controllers/suite_test.go
+++ controllers/suite_test.go
@@ -30,6 +30,7 @@ import (
 	logf "sigs.k8s.io/controller-runtime/pkg/log"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
 
+	jenkinsfilerunneriov1alpha1 "github.com/waveywaves/jenkinsfile-runner-operator/api/v1alpha1"
 	jenkinsiov1alpha1 "github.com/waveywaves/jenkinsfile-runner-operator/api/v1alpha1"
 	// +kubebuilder:scaffold:imports
 )
@@ -66,6 +67,9 @@ var _ = BeforeSuite(func(done Done) {
 	Expect(err).NotTo(HaveOccurred())
 
 	err = jenkinsiov1alpha1.AddToScheme(scheme.Scheme)
+	Expect(err).NotTo(HaveOccurred())
+
+	err = jenkinsfilerunneriov1alpha1.AddToScheme(scheme.Scheme)
 	Expect(err).NotTo(HaveOccurred())
 
 	// +kubebuilder:scaffold:scheme
