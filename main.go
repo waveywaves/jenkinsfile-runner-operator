824b40b6c52938ba80eaf4a51b03c825d98b3a3e
Vibhav Bobade <vibhav.bobde@gmail.com>
2020-11-19 05:56:28 +0530

    Remove RunnerImage, update Readme

--- main.go
+++ main.go
@@ -83,14 +83,6 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "Run")
 		os.Exit(1)
 	}
-	if err = (&controllers.RunnerImageReconciler{
-		Client: mgr.GetClient(),
-		Log:    ctrl.Log.WithName("controllers").WithName("RunnerImage"),
-		Scheme: mgr.GetScheme(),
-	}).SetupWithManager(mgr); err != nil {
-		setupLog.Error(err, "unable to create controller", "controller", "RunnerImage")
-		os.Exit(1)
-	}
 	// +kubebuilder:scaffold:builder
 
 	setupLog.Info("starting manager")
